40acebf027e2cff3c9015be129de25ccee379dd0
Nishanthkuppusamy12 <nishanthke12@gmail.com>
2022-11-10 08:39:09 +0530

    Commit in FilterConcepts

--- Trainning/src/com/stream/API/FilterConcept.java
+++ Trainning/src/com/stream/API/FilterConcept.java
@@ -8,15 +8,15 @@ public class FilterConcept {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Integer> al = new ArrayList<Integer>();
+		ArrayList<Integer> marks = new ArrayList<Integer>();
 		
-		al.add(0);
-		al.add(20);
-		al.add(15);
-		al.add(10);
-		al.add(5);
-		System.out.println(al);
-		List<Integer> l2=al.stream().filter(i->i%2==0).collect(Collectors.toList());
+		marks.add(0);
+		marks.add(20);
+		marks.add(15);
+		marks.add(10);
+		marks.add(5);
+		System.out.println(marks);
+		List<Integer> l2=marks.stream().map(i->i+10).collect(Collectors.toList());
         System.out.println(l2);
 	}
 
